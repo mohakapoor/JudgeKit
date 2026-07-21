3352d05aa28f2a8e7278ad9c179c623c067ea9e5
mohakkapoor4 <mohak.kapoor.v@gmail.com>
2026-07-22 00:52:34 +0530

    batch load questions

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+import json
+
 @dataclass
 class RetrievalInput:
     query: str
@@ -24,3 +26,22 @@ class GenerationOutput:
     faithfulness_reasoning: str 
     relevance_score: float
     relevance_reasoning: str
+
+
+def batch_load_questions(count,PATH):
+    with open(PATH,"r",encoding="utf-8") as f:
+        data = json.load(f)
+
+        retrieval_list = []
+        generation_list = []
+
+        for i in range(count):
+            ques = data["question"][i]
+            contexts = data["contexts"][i]
+            ground_truth = data["ground_truth"][i]
+            response = data["responses"][i][0]["text"]  
+            retrieval_list.append(RetrievalInput(ques,contexts,ground_truth))
+            generation_list.append(GenerationInput(ques,contexts,response))
+
+    return retrieval_list,generation_list
+
