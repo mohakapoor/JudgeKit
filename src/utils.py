88d6491502cab031cc72b23d7f7309e1c2a5a38d
mohakkapoor4 <mohak.kapoor.v@gmail.com>
2026-07-22 00:45:23 +0530

    updated classes for generation and retrieval

--- src/utils.py
+++ src/utils.py
@@ -6,13 +6,11 @@ class RetrievalInput:
     ground_truth: str 
 
 @dataclass
-class GenerationOutput:
-    faithfulness_score: float
-    faithfulness_reasoning: str 
-    relevance_score: float
-    relevance_reasoning: str
-
-
+class RetrievalOutput:
+    precision_score: float
+    precision_reasoning: str
+    recall_score: float
+    recall_reasoning: str
 
 @dataclass
 class GenerationInput:
@@ -20,3 +18,9 @@ class GenerationInput:
     contexts: list[str]
     response: str 
 
+@dataclass
+class GenerationOutput:
+    faithfulness_score: float
+    faithfulness_reasoning: str 
+    relevance_score: float
+    relevance_reasoning: str
