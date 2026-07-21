d8801bf5d21bfeeec616ce050bfea0d52836b611
mohakkapoor4 <mohak.kapoor.v@gmail.com>
2026-07-22 00:56:03 +0530

    batch load questions

--- src/utils.py
+++ src/utils.py
@@ -32,16 +32,20 @@ def batch_load_questions(count,PATH):
     with open(PATH,"r",encoding="utf-8") as f:
         data = json.load(f)
 
-        retrieval_list = []
-        generation_list = []
+        retrieval_inputs = []
+        generation_inputs = []
 
         for i in range(count):
             ques = data["question"][i]
             contexts = data["contexts"][i]
             ground_truth = data["ground_truth"][i]
             response = data["responses"][i][0]["text"]  
-            retrieval_list.append(RetrievalInput(ques,contexts,ground_truth))
-            generation_list.append(GenerationInput(ques,contexts,response))
+            retrieval_inputs.append(RetrievalInput(ques,contexts,ground_truth))
+            generation_inputs.append(GenerationInput(ques,contexts,response))
 
-    return retrieval_list,generation_list
+    return retrieval_inputs,generation_inputs
 
+def save_results(retrieval_results,generation_results,PATH):
+    with open(PATH,"w",encoding="utf-8") as f:
+        json.dump(retrieval_results,f)
+        json.dump(generation_results,f)
