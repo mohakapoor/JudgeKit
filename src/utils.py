9d442d944711fa95451f95312762a6f83218329c
mohakkapoor4 <mohak.kapoor.v@gmail.com>
2026-07-24 18:28:26 +0530

    updated loader to take in account default count

--- src/utils.py
+++ src/utils.py
@@ -64,11 +64,13 @@ def format_contexts(raw_contexts):
             formatted.append(str(c))
     return formatted
 
-def batch_load_questions(PATH,count=5):
+def batch_load_questions(PATH, count=None):
     with open(PATH,"r",encoding="utf-8") as f:
         data = json.load(f)
 
         inputs = []
+        if count is None:
+            count = len(data["question"])
 
         for i in range(count):
             ques = data["question"][i]
@@ -92,13 +94,13 @@ def random_ques_loader():
 
     return e
 
-def save_results(eval_res, PATH):
+def save_results(eval_res, path):
     combined_data = []
     
     for e in eval_res:
         combined_data.append(asdict(e))
 
-    with open(PATH, "w", encoding="utf-8") as f:
+    with open(path, "w", encoding="utf-8") as f:
         json.dump(combined_data, f, indent=4)
 
 
