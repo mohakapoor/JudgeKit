2cfb6ac8955887ba8ad0363226347632e2d49238
mohakkapoor4 <mohak.kapoor.v@gmail.com>
2026-07-24 16:58:15 +0530

    updated orchestrator to use hit_at_k and mrr

--- src/orchestrator.py
+++ src/orchestrator.py
@@ -1,6 +1,7 @@
 from src.utils import EvalInput,EvalOutput
 from src.prompt_builder import build_retrieval_prompt,build_generation_prompt
 from src.agents import retrieval_eval_agent,generation_eval_agent
+from src.deterministic import calc_metrics
 
 def orchestrator(data:EvalInput):
     ret_prompt = build_retrieval_prompt(data)
@@ -8,6 +9,10 @@ def orchestrator(data:EvalInput):
 
     ret_metrics = retrieval_eval_agent(ret_prompt)
     gen_metrics = generation_eval_agent(gen_prompt)
+
+    h,k = calc_metrics(data)
+    ret_metrics.hit_at_k = h
+    ret_metrics.mrr = k
 
     return EvalOutput(
         retrieval_metrics=ret_metrics,
