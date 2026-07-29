22fe8e4008d85632f20b5081b1779fb438c8fcb1
mohakkapoor4 <mohak.kapoor.v@gmail.com>
2026-07-29 22:10:06 +0530

    guard rails :

--- src/prompt_builder.py
+++ src/prompt_builder.py
@@ -2,16 +2,16 @@ from src.utils import EvalInput
 
 def build_retrieval_prompt(data:EvalInput) -> str:
     return f"""
-    Query: {data.query}
-    Context: {data.contexts}
+    <user_query>{data.query}</user_query>
+    <context>{data.contexts}</context>
     Ground Truth: {data.ground_truth}
     """
 
 def build_generation_prompt(data:EvalInput) -> str:
     return f"""
-    Query: {data.query}
-    Context: {data.contexts}
-    Response: {data.response}
+    <user_query>{data.query}</user_query>
+    <context>{data.contexts}</context>
+    <response>{data.response}</response>
     Ground Truth: {data.ground_truth}
     """
     
